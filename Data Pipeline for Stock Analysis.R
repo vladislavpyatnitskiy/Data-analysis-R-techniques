ec3efef3ff4afe78429d8fb0429b67d89c895816
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-13 17:10:06 +0500

    Update Data Pipeline for Stock Analysis.R
    Optimised script:
    
    • Renamed variables
    • Fixed bugs
    • Transform data as time series
    • Added automatic NA reduction

--- Data Pipeline for Stock Analysis.R
+++ Data Pipeline for Stock Analysis.R
@@ -1,24 +1,23 @@
-df.rus.maker1 <- function(x){ l <- NULL # Define name for Data Frame
+df.creator <- function(x){ l <- NULL # Define name for Data Frame
 
   l.names <- sapply(substitute(x),deparse)[-1] # Subtract names for columns
-
+  
   for (n in 1:length(x)){ s <- x[[n]] # Each Data Frame
     
-    if (is.null(l)){ l <- as.data.frame(s) # Join to Data frame
+    if (is.null(l)){ l <- as.data.frame(s) } else { # Join to Data frame
       
-      colnames(l)[2] <- l.names[n] } else { v <- as.data.frame(s)
-        
-        colnames(v)[2] <- l.names[n]  # Give name
-        
-        l <- merge(l, v, by = "Date") } } # Merge with other columns
+      l <- merge(l, as.data.frame(s), by = "Date") } } # Merge with  columns
   
-  rownames(l) <- l[,1] # Assign Dates as row names
+  rownames(l) <- l[,1] # Move Dates column to row names
   
-  l <- l[,-1] # Reduce Dates column
+  l <- l[,-1] # Reduce Dates column from main Data Frame
   
-  l # Display
+  l <- l[apply(l, 1,function(x) all(!is.na(x))),] # Reduce rows with na
+  
+  colnames(l) <- l.names # Give column names
+  
+  as.timeSeries(l) # Display
 }
-Nornickel.df <-df.rus.maker1(list(GMKN, Aluminium, Brent, Cattle, Copper, Gas,
-                                  Gold, Hogs, Nickel, Palladium, Platinum,
-                                  Rice, Silver, Soybeans, Sugar, USDRUB, Wheat,
-                                  Zinc)) # Test
+Nornickel.df <- df.creator(list(GMKN, Aluminium, Brent, Cattle, Copper, Gas,
+                                Gold, Hogs, Nickel, Palladium, Platinum, Rice,
+                                Silver, Soybeans, Sugar, USDRUB, Wheat, Zinc)) 

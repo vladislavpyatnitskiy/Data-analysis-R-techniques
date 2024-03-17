beea2dffaec4f14fa0aed5c35e2f5124a450eae2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-17 09:58:33 +0500

    Create Compounding Effect.R
    Added script to create data frame showing the compounding effect

---
+++ Compounding Effect.R
@@ -0,0 +1,20 @@
+# Compounding Effect
+compound.percentage.diff <- function(x, y = NULL, exp = F){
+  
+  if (is.null(y)){ y <-c(1, 10, 100, 1000, 10000) } # Time Periods
+  
+  D <- NULL # Store values
+  
+  for (n in 1:length(y)){ # Calculate via discrete or continuous way
+    
+    if (isFALSE(exp)){ D <- rbind(D, cbind((1 + x) ^ y[n], (1 - x) ^ y[n])) }
+    
+    else { D <- rbind(D,cbind(exp(x * y[n]), exp(-x * y[n]))) } }
+  
+  # Assign column names as percentages and row names as time periods
+  colnames(D) <- c(sprintf("+ %s %%", x * 100), sprintf("- %s %%", x * 100))
+  rownames(D) <- y 
+  
+  round(D, 8) # Display
+}
+compound.percentage.diff(0.001, exp = F) # Test

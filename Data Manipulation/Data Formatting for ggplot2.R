a37131baa3077af6280c7701fecc4d2fedc93b65
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-02 12:26:59 +0500

    Update Data Formatting for ggplot2.R
    Updated and optimised script:
    
    • Reduced excessive lines
    • Renamed variables
    • Edited comments
    • Combined FOR loops

--- Data Manipulation/Data Formatting for ggplot2.R
+++ Data Manipulation/Data Formatting for ggplot2.R
@@ -1,31 +1,22 @@
 data.transformation.ggplot <- function(x){ # Format data for ggplot2 plots
   
-  df0 <- NULL # Format Dates column
+  I <- NULL # Group tickers, indices or portfolio names
   
-  for (m in 1:nrow(x)){ for (n in 1:(ncol(x)-1)){ df0 <- rbind(df0, x[m,1]) } }
+  for (n in 2:(length(colnames(x)))){ I <- rbind(I, colnames(x)[n]) }
   
-  df11 <- NULL # Group tickers, indices or portfolio names
+  R <- NULL # Group returns
+  d <- NULL # Format Dates column
   
-  for (m in 1:nrow(x)){ df1 <- NULL # Create column with 
+  for (m in 1:nrow(x)){ # Join Returns in first loop and dates in second one
     
-    for (n in 2:(length(colnames(x)))){ # Collect all but the first one
-      
-      df1 <- rbind(df1, colnames(x)[n]) } # Collect all names from columns
+    for (n in 2:length(colnames(x))){ R <- rbind(R, x[m,n] * 100) } 
     
-    df11 <- rbind(df11, df1) } # Join repeating names of tickers and indices
+    for (n in 1:(ncol(x) - 1)){ d <- rbind(d, x[m,1]) } } 
   
-  df22 <- NULL # Group returns
+  D <- data.frame(d, I, R) # Join all 3 columns: Dates, Indices and Returns
   
-  for (m in 1:nrow(x)){ df2 <- NULL # Take value from each row from 2nd column 
-    
-    for (n in 2:length(colnames(x))){ df2 <- rbind(df2, x[m,n] * 100) }
-    
-    df22 <- rbind(df22, df2) } # Join return values 
+  colnames(D) <- c("Date", "Series", "Return") # Column names
   
-  df.final <- data.frame(df0, df11, df22) # Join all 3 columns
-  
-  colnames(df.final) <- c("Date", "Series", "Return") # Column names
-  
-  df.final # Display Data Frame
+  D # Display Data Frame
 }
-data.transformation.ggplot(test.plt.comp) # Test
+data.transformation.ggplot(test.plt.comp)

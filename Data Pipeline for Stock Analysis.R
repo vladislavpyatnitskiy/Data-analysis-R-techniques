ea38c02b2a0a83c605c1c726150b2cde545895b5
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-21 13:20:51 +0500

    Update Data Pipeline for Stock Analysis.R
    Optimised script:
    
    • Added necessary library of timeSeries
    • Reduced excessive line
    • Edited comments

--- Data Pipeline for Stock Analysis.R
+++ Data Pipeline for Stock Analysis.R
@@ -1,23 +1,25 @@
-df.creator <- function(x){ l <- NULL # Define name for Data Frame
+library(timeSeries) # Library
 
-  l.names <- sapply(substitute(x),deparse)[-1] # Subtract names for columns
+df.creator <- function(x){ # Bind Columns into Data Frame with Original Names
+  
+  l <- NULL 
   
   for (n in 1:length(x)){ s <- x[[n]] # Each Data Frame
-    
-    if (is.null(l)){ l <- as.data.frame(s) } else { # Join to Data frame
-      
-      l <- merge(l, as.data.frame(s), by = "Date") } } # Merge with  columns
   
+    if (is.null(l)){ l <- as.data.frame(s) } # Join to Data frame
+  
+    else { l <- merge(l, as.data.frame(s), by = "Date") } } # Merge columns
+    
   rownames(l) <- l[,1] # Move Dates column to row names
   
   l <- l[,-1] # Reduce Dates column from main Data Frame
   
-  l <- l[apply(l, 1,function(x) all(!is.na(x))),] # Reduce rows with na
+  l <- l[apply(l, 1, function(x) all(!is.na(x))),] # Reduce rows with na
   
-  colnames(l) <- l.names # Give column names
+  colnames(l) <- sapply(substitute(x), deparse)[-1] # Give names to columns 
   
   as.timeSeries(l) # Display
 }
-Nornickel.df <- df.creator(list(GMKN, Aluminium, Brent, Cattle, Copper, Gas,
-                                Gold, Hogs, Nickel, Palladium, Platinum, Rice,
-                                Silver, Soybeans, Sugar, USDRUB, Wheat, Zinc)) 
+df.creator(list(GMKN, Aluminium, Brent, Cattle, Copper, Gas,
+                Gold, Hogs, Nickel, Palladium, Platinum, Rice,
+                Silver, Soybeans, Sugar, USDRUB, Wheat, Zinc)) 

675b2eb8e9a50b35db944b91e106a9ef36a48567
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-26 10:19:25 +0500

    Update Vector Doubler.R
    Eliminated unnecessary WHILE loop

--- Data Manipulation/Vector Doubler.R
+++ Data Manipulation/Vector Doubler.R
@@ -2,11 +2,7 @@ vector.doubler <- function(x){ # Transform vector into two column data frame
   
   l <- NULL # Store values
   
-  for (n in 1:length(x)){ while (x[n] != x[length(x)]){ # Add values
-    
-      l <- rbind.data.frame(l, cbind(x[n], x[n + 1])) # Till last number
-    
-    break } } # Stop when cycle is over
+  for (n in 1:(length(x)-1)){ l <- rbind.data.frame(l, cbind(x[n], x[n + 1])) }
   
   l # Display
 }

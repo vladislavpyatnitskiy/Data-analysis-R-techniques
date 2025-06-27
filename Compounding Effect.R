95dece15daf68cc57212667076eff5f1871c03f4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-27 10:16:03 +0500

    Update Compounding Effect.R
    Rewrote IF-ELSE statements

--- Compounding Effect.R
+++ Compounding Effect.R
@@ -1,16 +1,15 @@
-# Compounding Effect
-compound.percentage.diff <- function(x, y = NULL, exp = F){
+compound.percentage.diff <- function(x, y=NULL, exp=F){ # Compounding Effect
   
-  if (is.null(y)){ y <-c(1, 10, 100, 1000, 10000) } # Time Periods
+  if (is.null(y)){ y <- c(1, 10, 100, 1000, 10000) } # Time Periods
   
   D <- NULL # Store values
   
   for (n in 1:length(y)){ # Calculate via discrete or continuous way
     
-    if (isFALSE(exp)){ D <- rbind(D, cbind((1 + x) ^ y[n], (1 - x) ^ y[n])) }
+    if (exp){ D <- rbind(D,cbind(exp(x * y[n]), exp(-x * y[n]))) }
     
-    else { D <- rbind(D,cbind(exp(x * y[n]), exp(-x * y[n]))) } }
-  
+    else { D <- rbind(D, cbind((1 + x) ^ y[n], (1 - x) ^ y[n])) } }
+    
   # Assign column names as percentages and row names as time periods
   colnames(D) <- c(sprintf("+ %s %%", x * 100), sprintf("- %s %%", x * 100))
   rownames(D) <- y 

0e5975277fec815335bdf0553634bd752343cab1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-29 11:35:33 +0500

    Update Time Period Calculator.R
    Replaced IF-ELSE statements with IFELSE function

--- Time Period Calculator.R
+++ Time Period Calculator.R
@@ -1,10 +1,8 @@
 date.calculator <- function(s = NULL, e = NULL, words = T,
-                            script = "Time period is %s days"){
+                            text = "Time period is %s days"){
   
-  if (isTRUE(words)){ # Make string with text; add %s for number 
-    
-    sprintf(script, as.numeric(as.Date(e) - as.Date(s))) } else {
-      
-      as.numeric(as.Date(e) - as.Date(s)) } # Show number as numeric
+  D <- as.numeric(as.Date(e) - as.Date(s)) # Calculate Number of days
+  
+  ifelse(words == T, sprintf(text, D), D) # Message or just days
 }
 date.calculator(s = "1976-06-04", e = "2024-02-16") # Test

15449a01ff36a925b60b2c754a252b63eb56dd07
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-01 14:45:08 +0500

    Update Unique Correlation Values Number.R
    Optimised script by replacing PRINT with RETURN what enables to get rid of ELSE statement

--- Unique Correlation Values Number.R
+++ Unique Correlation Values Number.R
@@ -1,8 +1,8 @@
 # Calculate umber of unique values for correlation or covariance
 unique.values.number <- function(x){ 
   
-  if (x < 2){ print("Please type any value bigger than 1") } else {
+  if (x < 2) return("Please type any value bigger than 1") 
     
-    x ^ 2 - sum(seq(x)) } # Calculate number
+  x ^ 2 - sum(seq(x)) # Calculate number
 }
 unique.values.number(32) # Test

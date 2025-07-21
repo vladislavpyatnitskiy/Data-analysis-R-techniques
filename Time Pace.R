9ca3e0677843eddbf85dd851f0a62e5dc99c05ff
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-21 11:39:04 +0500

    Update Time Pace.R
    Optimised script:
    
    • Reduced excessive lines and ELSE statement

--- Time Pace.R
+++ Time Pace.R
@@ -1,9 +1,9 @@
 time.pace <- function(x, y, e = F){ # Period when growing values converge
   
-  if (y[1] == y[2] || y[1] <= y[2]){ return("Convergence will not happen") }
+  if (y[1] <= y[2]) return(message("Convergence will not happen")) 
   
-  if (e){ log(x[1] / x[2]) / -(y[1] - y[2]) } # Discrete or Continuous
+  if (e) return(log(x[1] / x[2]) / -(y[1] - y[2])) # Discrete or Continuous
   
-  else { log(x[1] / x[2]) / log((1 + y[2]) / (1 + y[1])) }
+  log(x[1] / x[2]) / log((1 + y[2]) / (1 + y[1]))
 }
 time.pace(c(20, 1000), c(0.1, 0.1), e = T) # Test

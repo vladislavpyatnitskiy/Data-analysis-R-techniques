b6b5ecaab40fc664302659143a241aad388db802
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-22 11:38:06 +0500

    Update Time Period Calculator.R
    Added message occurring when typed end date is earlier than start date

--- Time Period Calculator.R
+++ Time Period Calculator.R
@@ -1,5 +1,7 @@
 date.calculator <- function(s = NULL, e = NULL, words = T,
                             text = "Time period is %s days"){
+
+  if (s > e) return(message("End date cannot be earlier than Start date"))
   
   D <- as.numeric(as.Date(e) - as.Date(s)) # Calculate Number of days
   
